64b055cef337a58ae7a6baae5cf82b9e53929b56
Owais Nazir Dar <63143570+owaisnazir110928@users.noreply.github.com>
2023-10-30 13:17:00 +0530

    ..

--- server.js
+++ server.js
@@ -3,45 +3,40 @@ const fs = require("fs");
 
 async function addWatermarkToPdf(pdfPath, text, outputPath) {
   const pdfBuffer = fs.readFileSync(pdfPath);
-  const pdfDoc = await PDFDocument.load(pdfBuffer, { ignoreEncryption: true });
-
-  const customFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-  const textSize = 35
-  const textWidth = customFont.widthOfTextAtSize(text, textSize)
-  const textHeight = customFont.heightAtSize(textSize)
+  const pdfDoc = await PDFDocument.load(pdfBuffer);
+  const textWidth = 250;
+  const textHeight = 45;
 
   for (let i = 0; i < pdfDoc.getPageCount(); i++) {
     const page = pdfDoc.getPage(i);
     const { width, height } = page.getSize();
     const rotationAngle = degrees(20.25);
     const fontSize = 16;
-    // Top Left Position
+
     page.drawText(watermarkText, {
       x: 50,
       y: height - 175,
       size: fontSize,
-      color: rgb(0.5, 0.5, 0.5),
-      opacity: 0.5,
+      color: rgb(0, 0, 0),
+      opacity: 0.14,
       rotate: rotationAngle,
     });
 
-    // Middle Position
     page.drawText(watermarkText, {
       x: width / 2 - textWidth / 2,
       y: height / 2 - textHeight / 2,
       size: fontSize,
-      color: rgb(0.5, 0.5, 0.5),
-      opacity: 0.5,
+      color: rgb(0, 0, 0),
+      opacity: 0.14,
       rotate: rotationAngle,
     });
 
-    // Bottom Right Position
     page.drawText(watermarkText, {
       x: width - 300,
       y: 100,
       size: fontSize,
-      color: rgb(0.5, 0.5, 0.5),
-      opacity: 0.5,
+      color: rgb(0, 0, 0),
+      opacity: 0.14,
       rotate: rotationAngle,
     });
   }
@@ -49,14 +44,17 @@ async function addWatermarkToPdf(pdfPath, text, outputPath) {
   const modifiedPdfBytes = await pdfDoc.save();
   fs.writeFileSync(outputPath, modifiedPdfBytes);
 
-  console.log(
-    "Rotated text watermarks added to different positions on the PDF successfully!"
-  );
+  console.log(" watermarks added to PDF successfully!");
 }
 
 const pdfPath = "input.pdf";
-const watermarkText =
-  "Accessed by: Rahi Ahriwar\nEmail ID :  raviahirwar660@gmail.com\nPurchased on :  01-10-2023";
+const email = "owaisnazir110928@gmail.com";
+const userName = "Owais Nazir Dar";
+const utcdate = new Date("01/10/2023");
+const date = utcdate.toLocaleDateString();
+
+const watermarkText = `Accessed by: ${userName}\nEmail ID: ${email}\nPurchased on: ${date}`;
+
 const outputPath = "output.pdf";
 
 addWatermarkToPdf(pdfPath, watermarkText, outputPath);
