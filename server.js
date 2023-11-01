7d28d47ae7917c3d9ac17f7b3b6bb8f309c17225
Owais Nazir Dar <63143570+owaisnazir110928@users.noreply.github.com>
2023-11-01 14:17:40 +0530

    ..

--- server.js
+++ server.js
@@ -1,38 +1,7 @@
 const { PDFDocument, rgb, degrees } = require("pdf-lib");
 const fs = require("fs");
 
-async function addWatermarkToPdf(pdfPath, text, svgPath, outputPath) {
-  const pdfBuffer = fs.readFileSync(pdfPath);
-  const pdfDoc = await PDFDocument.load(pdfBuffer);
 
-  const fontSize = 16;
-  const opacity = 0.2;
-
-  for (let i = 0; i < pdfDoc.getPageCount(); i++) {
-    const page = pdfDoc.getPage(i);
-    const { width, height } = page.getSize();
-
-    const addWatermark = (x, y, content) => {
-      page.drawText(content, {
-        x,
-        y,
-        size: fontSize,
-        color: rgb(0.659, 0.659, 0.659),
-        opacity,
-        rotate: degrees(20.25),
-      });
-    };
-
-    addWatermark(width / 2 - 125, height - 175, text);
-    addWatermark(width / 2 - 125, height / 2 - 22.5, text);
-    addWatermark(width / 2 - 125, 75, text);
-  }
-
-  const modifiedPdfBytes = await pdfDoc.save();
-  fs.writeFileSync(outputPath, modifiedPdfBytes);
-
-  console.log("Watermarks and SVG image added to PDF successfully!");
-}
 
 // Usage example:
 const pdfPath = "input.pdf";
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { PDFDocument, rgb, degrees } from "pdf-lib";
+
+import profilePNG from "../profile.png";
+import emailPNG from "../email.png";
+import tagPNG from "../tag.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const outputPath = "output.pdf";
+  const email = "owaisnazir110928@gmail.com";
+  const userName = "Owais Nazir Dar";
+  const utcdate = new Date("01/10/2023");
+  const date = utcdate.toLocaleDateString();
+  const watermarkText = ` ${userName}\n ${email}\n ${date}`;
+
+  async function addWatermarkToPdf(text) {
+    const pdfUrl = "https://pdf-lib.js.org/assets/with_update_sections.pdf";
+    const pdfBytes = await fetch(pdfUrl).then((res) => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(pdfBytes);
+
+    const profilePngResponse = await fetch(profilePNG);
+    const profilePngBytes = await profilePngResponse.arrayBuffer();
+    const profileImg = await pdfDoc.embedPng(profilePngBytes);
+
+    const emailPngResponse = await fetch(emailPNG);
+    const emailPngBytes = await emailPngResponse.arrayBuffer();
+    const emailImg = await pdfDoc.embedPng(emailPngBytes);
+
+    const tagPngResponse = await fetch(tagPNG);
+    const tagPngBytes = await tagPngResponse.arrayBuffer();
+    const tagImg = await pdfDoc.embedPng(tagPngBytes);
+    const fontSize = 16;
+    const opacity = 0.2;
+
+    for (let i = 0; i < pdfDoc.getPageCount(); i++) {
+      const page = pdfDoc.getPage(i);
+      const { width, height } = page.getSize();
+
+      const addWatermark = (x, y, content) => {
+        page.drawText(content, {
+          x,
+          y,
+          size: fontSize,
+          color: rgb(0.659, 0.659, 0.659),
+          opacity,
+          rotate: degrees(25),
+        });
+      };
+
+      addWatermark(width / 2 - 125, height - 175, text);
+      addWatermark(width / 2 - 125, height / 2 - 22.5, text);
+      addWatermark(width / 2 - 125, 75, text);
+
+      const addPngs = (x, y, img) => {
+        page.drawImage(img, {
+          x,
+          y,
+        });
+      };
+      addPngs(width / 2 - 155, height - 190, profileImg);
+      addPngs(width / 2 - 142.5, height - 215, emailImg);
+      addPngs(width / 2 - 130, height - 240, tagImg);
+
+      addPngs(width / 2 - 155, height / 2 - 37.5, profileImg);
+      addPngs(width / 2 - 142.5, height / 2 - 62.5, emailImg);
+      addPngs(width / 2 - 130, height / 2 - 87.5, tagImg);
+
+      addPngs(width / 2 - 155, 60, profileImg);
+      addPngs(width / 2 - 142.5, 35, emailImg);
+      addPngs(width / 2 - 130, 10, tagImg);
+    }
+
+    const modifiedPdfBytes = await pdfDoc.save();
+    const modifiedPdfBlob = new Blob([modifiedPdfBytes], {
+      type: "application/pdf",
+    });
+    const modifiedPdfUrl = URL.createObjectURL(modifiedPdfBlob);
+    window.open(modifiedPdfUrl);
+
+    console.log("Watermarks and SVG image added to PDF successfully!");
+  }
+
+  useEffect(() => {
+    addWatermarkToPdf(watermarkText, outputPath);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        onClick={() => {
+          addWatermarkToPdf(watermarkText, outputPath);
+        }}
+      >
+        Click here
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
